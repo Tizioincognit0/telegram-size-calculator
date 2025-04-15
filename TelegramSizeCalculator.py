73075d45aaaed4a690a093dec10facd6c95d6110
Tizio Incognito <40031873+Tizioincognit0@users.noreply.github.com>
2025-04-15 16:49:30 +0200

    Fix wrong attribute

--- TelegramSizeCalculator.py
+++ TelegramSizeCalculator.py
@@ -2,7 +2,7 @@ from telethon.sync import TelegramClient
 from telethon.tl.types import MessageMediaDocument
 
 # Sostituisci questi valori con le tue credenziali API di Telegram
-api_id =
+api_id = 
 api_hash = ""
 channel_username = ""  # Inserisci l'username o ID del canale
 
@@ -16,7 +16,7 @@ async def get_total_size():
     async with client:
         async for message in client.iter_messages(channel_username):
             if message.file:
-                total_size += message.media.document.size  # Somma la dimensione del file
+                total_size += message.file.size  # Somma la dimensione del file
                 count += 1
 
     print(f"Numero totale di file: {count}")
